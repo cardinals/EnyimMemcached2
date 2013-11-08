3aa5abceda3ab574cc48ae82791b28d661c90b77
Attila Kisko <a@enyim.com>
2013-11-08 21:01:38 +0100

    Use AppVeyor's properties for build versioning (when available).

--- NLog/Properties/AssemblyInfo.cs
+++ NLog/Properties/AssemblyInfo.cs
@@ -22,19 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c963f7cf-c051-46a5-8396-f3b8bb73f074")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 #region [ License information          ]
 
 /* ************************************************************
