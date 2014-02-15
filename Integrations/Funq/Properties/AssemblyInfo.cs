12f8d45b021d50d2fc5ffeb2d2b4f9235153e7c5
Attila Kisko <a@enyim.com>
2014-02-15 17:11:08 +0100

    Create a Funq specific integration package.

---
+++ Integrations/Funq/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Enyim.Caching.Memcached.Integrations.Funq")]
+[assembly: AssemblyProduct("Enyim.Caching.Memcached.Integrations.Funq")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("083e1308-56df-464a-930a-fbca46fb912f")]
+
+#region [ License information          ]
+
+/* ************************************************************
+ *
+ *    Copyright (c) Attila Kiskó, enyim.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * ************************************************************/
+
+#endregion
